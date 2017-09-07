ab3e76e5e78b9f7e9b68dfe0f1f341e8997ba82d
BoB Tubocass <tubocass@gmail.com>
2017-09-07 16:31:54 -0500

    Removed the child transform "SpawnMouth" from MoM (and seemed to see a big boost in FPS). I think I fixed the food carrying problem in the last commit... somehow. only thing I can think of was removing the rigidbody from drones

--- HiveMind/Assets/_Scripts/Drone/Detection.cs
+++ HiveMind/Assets/_Scripts/Drone/Detection.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class Detection : MonoBehaviour 
 {
-	NetworkFarmer controller;
+	Unit_Base controller;
 	// Use this for initialization
 	void Start () 
 	{
-		controller = transform.parent.GetComponent<NetworkFarmer>();
+		controller = transform.parent.GetComponent<Unit_Base>();
 	}
 	
 	void OnTriggerEnter(Collider other)
