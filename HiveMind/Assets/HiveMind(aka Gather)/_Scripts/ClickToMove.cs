bae852b52d68ba77a53180802d1674dc234852f1
Tubocass <tubocass@gmail.com>
2018-09-18 19:40:20 -0500

    Added rest of cave generation tutorial script. added navmesh and off-mesh links.

---
+++ HiveMind/Assets/HiveMind(aka Gather)/_Scripts/ClickToMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] LayerMask mask;
+    MeshRenderer renderer;
+    bool hide = false;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        renderer = GetComponent<MeshRenderer>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click");
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100f, mask))
+            {
+                agent.SetDestination(hit.point);
+                Debug.Log(hit.point.ToString());
+            }
+        }
+
+        if (agent.isOnOffMeshLink)
+        {
+            if (!hide)
+            {
+                renderer.enabled = false;
+                hide = true;
+            }
+        }
+        else
+        {
+            if(hide)
+            {
+                renderer.enabled = true;
+                hide = false;
+            }
+        }
+    }
+}
