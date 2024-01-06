ad02a91f46aba57946915f8c8766b8da67246047
tubocass <tubocass@gmail.com>
2024-01-06 10:08:55 -0700

    LevelSetup spawns food bushes. Removed empty space from bush sprite. LevelSetup uses bounds of collider for spawn range (for now)

--- Gather/Assets/Gather/Scripts/LevelSetup.cs
+++ Gather/Assets/Gather/Scripts/LevelSetup.cs
@@ -4,21 +4,22 @@ namespace gather
 {
     public class LevelSetup : MonoBehaviour
     {
+        [SerializeField] GameObject FoodBushPrefab;
+        [SerializeField] int numFoodBushes = 20;
+        [SerializeField] float foodBushMinimumDist;
+        [SerializeField] Transform foodParent;
+        [Space]
+        [SerializeField] int numSpawnPoints = 8;
+        [SerializeField] float spawnPointMinimumDist;
         Vector2[] startLocations;
         int startIndex;
-        [SerializeField] GameObject locationPrefab;
-        [SerializeField] int numSpawnPoints = 8;
-        [SerializeField] float range = 500;
-        [SerializeField] float startLocationMinimumDist;
+        float xRange, yRange;
 
         private void Awake()
         {
-            //var children = transform.GetComponentsInChildren<Transform>();
-            //startLocations = new Vector2[children.Length - 1];
-            //for (int i = 0; i < startLocations.Length; i++) 
-            //{
-            //    startLocations[i] = children[i + 1].position;
-            //}
+            Collider2D bgCollider = GetComponent<Collider2D>();
+            xRange = bgCollider.bounds.extents.x;
+            yRange = bgCollider.bounds.extents.y;
         }
 
         public Vector2 GetStartLocation()
@@ -28,18 +29,46 @@ namespace gather
 
         public void Generate()
         {
+            GenerateSpawnPoints();
+            GenerateFood();
+        }
+
+        void GenerateSpawnPoints()
+        {
             startLocations = new Vector2[numSpawnPoints];
             Vector2 pos = Vector2.zero;
             for (int i = 0; i < numSpawnPoints; i++)
             {
                 do
                 {
-                    float x = Random.Range(-range, range);
-                    float y = Random.Range(-range, range);
+                    float x = Random.Range(-xRange, xRange);
+                    float y = Random.Range(-yRange, yRange);
                     pos = new Vector2(x, y);
-                } while (FailsMinimumDistanceCheck(pos, startLocations, startLocationMinimumDist, i));
+                } while (FailsMinimumDistanceCheck(pos, startLocations, spawnPointMinimumDist, i));
 
                 startLocations[i] = pos;
+            }
+        }
+
+        void GenerateFood()
+        {
+            Vector2[] foodLocations = new Vector2[numFoodBushes];
+            Vector2 pos = Vector2.zero;
+            for (int i = 0; i < numFoodBushes; i++)
+            {
+                do
+                {
+                    float x = Random.Range(-xRange, xRange);
+                    float y = Random.Range(-yRange, yRange);
+                    pos = new Vector2(x, y);
+                } while (FailsMinimumDistanceCheck(pos, foodLocations, foodBushMinimumDist, i));
+
+                foodLocations[i] = pos;
+            }
+
+            for (int i = 0; i < numFoodBushes; i++)
+            {
+                GameObject.Instantiate(FoodBushPrefab, foodLocations[i], Quaternion.identity, foodParent);
             }
         }
 
