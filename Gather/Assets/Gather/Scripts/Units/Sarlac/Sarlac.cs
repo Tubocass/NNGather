1ab5dbd4b8456fd9fcba150f8f9b7807163941eb
tubocass <tubocass@gmail.com>
2024-02-06 16:14:39 -0700

    deleted GameEvent class and objects

--- Gather/Assets/Gather/Scripts/Units/Sarlac/Sarlac.cs
+++ Gather/Assets/Gather/Scripts/Units/Sarlac/Sarlac.cs
@@ -2,15 +2,31 @@ namespace gather
 {
     public class Sarlac : Unit
     {
- 
+
         void Start()
         {
-        
+            TimeManager timeManager = FindFirstObjectByType<TimeManager>();
+            timeManager.Dusk.AddListener(WakeUp);
+            timeManager.Dawn.AddListener(StartSleep);
         }
 
         void Update()
         {
         
+        }
+
+        void StartSleep()
+        {
+            /*
+             * if at home then sleep, else move to home
+            */
+        }
+
+        void WakeUp()
+        {
+            /*
+             * play sound and animation, start hunting
+            */
         }
     }
 }
