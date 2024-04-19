b0ac129fccecb8a6ada87923ffd87afdb2d07019
tubocass <tubocass@gmail.com>
2024-04-18 21:23:23 -0600

    PopBar setup

--- Gather/Assets/Gather/Scripts/UI/UI Elements/PopulationBarController.cs
+++ Gather/Assets/Gather/Scripts/UI/UI Elements/PopulationBarController.cs
@@ -6,21 +6,27 @@ namespace Gather.UI
     public class PopulationBarController : MonoBehaviour
     {
         int total = 0;
+        float minLineWidth = 40f;
         protected TeamConfig[] teams;
         PopulationBarElement[] elements;
         VisualElement fillBar;
 
-        public virtual void SetTeams(TeamConfig[] teams)
+        private void Awake()
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
             fillBar = root.Q<VisualElement>(name: "PopulationBar");
             fillBar.Clear();
+        }
+
+        public virtual void SetTeams(TeamConfig[] teams)
+        {
             this.teams = teams;
             elements = new PopulationBarElement[teams.Length];
             for (int t = 0; t < elements.Length; t++)
             {
                 elements[t] = new PopulationBarElement();
                 elements[t].fillColor = teams[t].TeamColor;
+                elements[t].lineWidth = fillBar.contentRect.height > 0 ? fillBar.contentRect.height : minLineWidth;
                 fillBar.Add(elements[t]);
             }
         }
@@ -34,21 +40,10 @@ namespace Gather.UI
                 float width = fillBar.contentRect.width;
                 for (int t = 0; t < teams.Length; t++)
                 {
-
-                    if (t == 0)
-                    {
-                        elements[t].fillAmount = CalcTeamPercent(t) * width;
-                        edge += elements[t].fillAmount;
-                        //elements[t].style.width = edge / 2;
-
-                    } else
-                    {
-                        Vector2 start = new Vector2(edge/2, 0);
-                        elements[t].transform.position = start;
-                        elements[t].fillAmount = CalcTeamPercent(t) * width; ;
-                        edge += elements[t].fillAmount;
-                        //elements[t].style.width = edge / 2;
-                    }
+                    Vector2 start = new Vector2(edge/2, 0);
+                    elements[t].transform.position = start;
+                    elements[t].fillAmount = CalcTeamPercent(t) * width; ;
+                    edge += elements[t].fillAmount;
                 }
             }
         }
